ee2ea6bbc011b9f597911f964bd305036935915d
Elijah Triumph <eliyang256@outlook.com>
2024-11-22 15:16:33 +0300

    remove api

--- src/routes/api/auth/logout/+server.ts
+++
@@ -1,6 +0,0 @@
-import { json } from '@sveltejs/kit';
-
-export async function POST({ cookies }) {
-    cookies.delete('authToken', { path: '/' });
-    return json({ message: 'Logged out successfully' });
-}
